5dd3abb9e1e0a2e501a0731d6718ba85334f27f7
tomdoo77 <tomdoo79@gmail.com>
2020-11-07 22:09:49 +0100

    Added restock class

---
+++ Shop/DataTypes/Restock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shop.DataTypes
+{
+    public class Restock : StockEvent
+    {
+
+        public Restock(int amount, Client client, MagazineState magazineState, Product product) : base(amount, client, magazineState, product)
+        {
+        }
+    }
+}
