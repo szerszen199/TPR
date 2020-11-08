9a34c1f24364ba32ab35a2b2288f3340235fa52f
tomdoo77 <tomdoo79@gmail.com>
2020-11-08 19:52:12 +0100

    XML Fill Test

--- Shop/UnitTest/FillFromFileTest.cs
+++ Shop/UnitTest/FillFromFileTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Shop;
@@ -10,12 +11,13 @@ namespace UnitTests
     [TestClass]
     public class FillFromFileTest
     {
-        public DataRepository dataRepository = new DataRepository();
+        public DataRepository dataRepository = new DataRepository(new FillFromFile());
+
         [TestMethod]
         public void fillFromFile()
         {
             
-            DataRepository dataRepository = new DataRepository();
+            
 
             FillFromFile fillFromFile = new FillFromFile();
 
@@ -27,6 +29,30 @@ namespace UnitTests
             {
                 Console.WriteLine($"{lista[i]}");
             }
+        }
+
+        [TestMethod]
+        public void fillTest()
+        {
+
+            FillFromFile fillFromFile = new FillFromFile();
+            List<string> firstNames, surNames, guidList, costList, productNameList = new List<string>();
+
+
+            firstNames = fillFromFile.readElementFromFile("firstName");
+            surNames = fillFromFile.readElementFromFile("surName");
+            guidList = fillFromFile.readElementFromFile("guid");
+            costList = fillFromFile.readElementFromFile("cost");
+            productNameList = fillFromFile.readElementFromFile("productName");
+
+            dataRepository.FillClients();
+            dataRepository.FillProducts();
+            Assert.AreEqual(firstNames[0], dataRepository.GetClient(0).FirstName);
+            Assert.AreEqual(surNames[0], dataRepository.GetClient(0).SurName);
+            Assert.AreEqual(Guid.Parse(guidList[0]), dataRepository.GetProduct(Guid.Parse(guidList[0])).Guid);
+            Assert.AreEqual(double.Parse(costList[0], CultureInfo.InvariantCulture), dataRepository.GetProduct(Guid.Parse(guidList[0])).Cost);
+            Assert.AreEqual(productNameList[0], dataRepository.GetProduct(Guid.Parse(guidList[0])).ProductName);
+
         }
     }
 }
