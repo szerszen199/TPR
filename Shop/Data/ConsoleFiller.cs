b9fb085f9df209caea611bd340127b4e5c7700ab
tomdoo77 <tomdoo79@gmail.com>
2020-12-08 22:57:13 +0100

    Added console app

---
+++ Shop/Data/ConsoleFiller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Shop.Data.DataTypes;
+
+namespace Shop.Data
+{
+    class ConsoleFiller : IDataFiller
+    {
+        public void Fill(DataContext context)
+        {
+            context.clients.Add(new Client("Grzegorz", "Muszynski"));
+            context.clients.Add(new Client("Tomasz", "Wozniak"));
+
+            Guid Guid1 = Guid.Parse("3EA13DB8-DCB3-4ED9-805C-B88AA99AB5C5");
+            Guid Guid2 = Guid.Parse("B7810043-50A9-405D-97B0-19E8B0154B58");
+            Guid Guid3 = Guid.Parse("6D5A44F8-AE83-44C8-A15E-A4525450C907");
+
+            context.products.Add(Guid1, new Product(Guid1, 10.98, "Table"));
+            context.products.Add(Guid2, new Product(Guid2, 17.53, "Chair"));
+            context.products.Add(Guid3, new Product(Guid3, 35.04, "Desk"));
+
+            context.magazineStates.Add(new MagazineState(context.products[Guid1], 10));
+            context.magazineStates.Add(new MagazineState(context.products[Guid2], 5));
+            context.magazineStates.Add(new MagazineState(context.products[Guid3], 7));
+
+            context.stockEvents.Add(new Bill(4, context.clients[0], context.magazineStates[0], context.products[Guid1]));
+            context.stockEvents.Add(new Bill(1, context.clients[1], context.magazineStates[1], context.products[Guid2]));
+            context.stockEvents.Add(new Restock(5, context.magazineStates[1], context.products[Guid2]));
+
+
+        }
+    }
+}
--- Shop/Program.cs
+++ Shop/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Shop.Data;
+using System;
+using System.IO;
 
 namespace Shop
 {
@@ -6,7 +8,42 @@ namespace Shop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Operations: ");
+            Console.WriteLine("1. Export to JSON");
+            Console.WriteLine("2. Import from JSON");
+            int choice = Console.Read() - '0';
+
+            IDataRepository dataRepository = new DataRepository(new ConsoleFiller());
+            IDataRepository dataRepositoryDeserialized = new DataRepository(new ConsoleFiller());
+            dataRepository.Fill();
+            Serialize serialize = new Serialize();
+            Deserialize deserialize = new Deserialize();
+            switch (choice)
+            {
+                case 1:
+                    serialize.SerializeToJSON(dataRepository);
+                    Console.WriteLine("Successfully serialized!");
+                    break;
+                case 2:
+                    try
+                    {
+                        dataRepositoryDeserialized.SetAllClients(deserialize.JSONToClient("Clients.json"));
+                        dataRepositoryDeserialized.SetAllProducts(deserialize.JSONToProduct("Products.json"));
+                        dataRepositoryDeserialized.SetAllMagazineStates(deserialize.JSONToMagazineState("MagazineStates.json"));
+                        dataRepositoryDeserialized.SetAllStockEvents(deserialize.JSONToStockEvent("StockEvents.json"));
+                        Console.WriteLine("Successfully deserialized!");
+                    }
+                    catch (FileNotFoundException e)
+                    {
+                        Console.WriteLine("Error: " + e.GetType());
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
