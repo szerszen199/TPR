ddf3372ac240bf5aca80e04e34fd6976cc4defff
Grzegorz Muszyński <72755079+szerszen199@users.noreply.github.com>
2020-12-29 16:39:09 +0100

    Data for serialization classes

---
+++ zad2/Serialization/Serialization/Data/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Serialization.Data
+{
+    public class Class1 : ISerializable
+    {
+        public string Text { get; set; }
+        public DateTime DateTime { get; set; }
+        public double DoubleVal { get; set; }
+        public Class2 Class2 { get; set; }
+        public Class3 Class3 { get; set; }
+
+        public Class1(string text, DateTime dateTime, double doubleVal, Class2 class2, Class3 class3)
+        {
+            Text = text;
+            DateTime = dateTime;
+            DoubleVal = doubleVal;
+            Class2 = class2;
+            Class3 = class3;
+
+        }
+        public Class1(string text, DateTime dateTime, double doubleVal)
+        {
+            Text = text;
+            DateTime = dateTime;
+            DoubleVal = doubleVal;
+        }
+
+        public Class1()
+        {
+
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Text", Text);
+            info.AddValue("DateTime", DateTime);
+            info.AddValue("DoubleVal", DoubleVal);
+            info.AddValue("Class2", Class2);
+            info.AddValue("Class3", Class3);
+        }
+    }
+}
---
+++ zad2/Serialization/Serialization/Data/Class3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace Serialization.Data
+{
+    [Serializable]
+    public class Class3 : ISerializable
+    {
+        public string Text { get; set; }
+        public DateTime DateTime { get; set; }
+        public double DoubleVal { get; set; }
+        public Class1 Class1 { get; set; }
+        public Class2 Class2 { get; set; }
+
+        public Class3(string text, DateTime dateTime, double doubleVal, Class1 class1, Class2 class2)
+        {
+            Text = text;
+            DateTime = dateTime;
+            DoubleVal = doubleVal;
+            Class1 = class1;
+            Class2 = class2;
+
+        }
+        public Class3(string text, DateTime dateTime, double doubleVal)
+        {
+            Text = text;
+            DateTime = dateTime;
+            DoubleVal = doubleVal;
+        }
+        public Class3()
+        {
+
+        }
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Text", Text);
+            info.AddValue("DateTime", DateTime);
+            info.AddValue("DoubleVal", DoubleVal);
+            info.AddValue("Class1", Class1);
+            info.AddValue("Class2", Class2);
+        }
+    }
+}
