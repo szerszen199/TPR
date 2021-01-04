f55b846ecad4f5adb55a9879f1702243f89b47b4
Grzegorz Muszyński <72755079+szerszen199@users.noreply.github.com>
2021-01-04 18:57:44 +0100

    unnecessary trycatches

--- zad2/Serialization/Serialization/Serializer/SerializeContoller.cs
+++ zad2/Serialization/Serialization/Serializer/SerializeContoller.cs
@@ -5,36 +5,19 @@ namespace Serialization.Serializer
 {
     public class SerializeContoller
     {
-        static public Boolean SertializeObject(object myObject, string filename)
+        static public void SertializeObject(object myObject, string filename)
         {
             CustomSerializer serializer = new CustomSerializer();
             Stream stream = File.Open(filename, FileMode.Create);
-            try
-            {
-                serializer.Serialize(stream, myObject);
-            }
-            catch (System.InvalidCastException)
-            {
-                stream.Close();
-                return false;
-            }
+            serializer.Serialize(stream, myObject);
             stream.Close();
-            return true;
         }
         static public object DesertializeObject(string filename)
         {
             CustomSerializer serializer = new CustomSerializer();
             Stream stream = File.Open(filename, FileMode.Open);
             object objCopy = new object();
-            try
-            {
-                objCopy = serializer.Deserialize(stream);
-            }
-            catch (System.Collections.Generic.KeyNotFoundException)
-            {
-                stream.Close();
-                return objCopy;
-            }
+            objCopy = serializer.Deserialize(stream);
             stream.Close();
             return objCopy;
         }
